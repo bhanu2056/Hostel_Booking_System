6b4069bdbbd97241552ec8c4288b5561f4a08330
bhanu2056 <132937741+bhanu2056@users.noreply.github.com>
2023-10-05 13:07:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let butt = document.getElementById("btn");
+butt.textContent = "Contact Now";
+butt.style.color = "white"
+butt.style.backgroundColor = "rgba(255, 84, 62, 0.8)";
+butt.style.borderStyle = "solid";
+butt.classList.add("u");
+let butt1 = document.getElementById("bt");
+butt1.textContent = "Explore More";
+butt1.style.backgroundColor = "rgba(255, 84, 62, 0.8)";
+butt1.classList.add("custom-button")
+let butt2 = document.getElementById("btn1");
+butt2.textContent = "Contact";
+butt2.style.backgroundColor = "rgba(255, 84, 62, 0.8)";
+butt2.classList.add("d")
